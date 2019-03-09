b06af95975c2f9c9ef982bd39458ccb63e10fbf9
Kviatsinskaya Anastasia <anastasiakviatsinskaya@gmail.com>
2019-03-09 20:30:11 +0300

    added line wrapping

--- loci/src/main/java/loci/gui/Controller.java
+++ loci/src/main/java/loci/gui/Controller.java
@@ -1,5 +1,8 @@
 package loci.gui;
 
+import javafx.beans.property.SimpleStringProperty;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -10,8 +13,11 @@ import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyCode;
 import javafx.scene.input.KeyEvent;
 import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.GridPane;
 import javafx.scene.text.Text;
+import javafx.stage.StageStyle;
 import loci.desk.Desk;
 import loci.entity.Card;
 import loci.exception.CustomException;
@@ -20,6 +26,7 @@ import loci.parser.DatabaseCreator;
 import loci.traning.EnterWord;
 
 import java.net.URL;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 
@@ -28,6 +35,11 @@ public class Controller implements Initializable {
     //Tab of "Training"
     @FXML
     private Tab trainTab;
+    @FXML
+    private AnchorPane trainingPane;
+    @FXML
+    private AnchorPane trainingSettingsPane;
+    //Training
     @FXML
     private Text resultText;
     @FXML
@@ -44,6 +56,21 @@ public class Controller implements Initializable {
     private Button variantC_Button;
     @FXML
     private Button variantD_Button;
+    @FXML
+    private Button goToSettingsButton;
+    @FXML
+    private GridPane gridForButtons;
+    //Settings for training
+    @FXML
+    private RadioButton isTraining1Set;
+    @FXML
+    private RadioButton isTraining2Set;
+    @FXML
+    private RadioButton isTraining3Set;
+    @FXML
+    private RadioButton isTraining4Set;
+    @FXML
+    private Button startTrainingButton;
 
     //Tab of "Desk"
     @FXML
@@ -60,12 +87,11 @@ public class Controller implements Initializable {
     private TableColumn statusTableColumn;
     @FXML
     private ImageView deskQuestionImage;
-    @FXML
-    private GridPane gridForButtons;
 
     EnterWord enterWord = new EnterWord();
     Card card;
-    Image startImage = new Image("images/question.png");
+    String startPath = new String("images/start/question.png");
+    Image startImage = new Image(startPath);
 
     //Tab of "Help"
     @FXML
@@ -94,7 +120,6 @@ public class Controller implements Initializable {
     public void getImage(MouseEvent event){
         Card selectedCard;
         selectedCard = tableViev.getSelectionModel().getSelectedItem();
-        selectedCard.getPicturePath();
         deskQuestionImage.setImage(new Image(selectedCard.getPicturePath().substring(19)));
     }
 
@@ -120,10 +145,24 @@ public class Controller implements Initializable {
         }
     }
 
+    public void goToTraining(ActionEvent event){
+
+        trainingSettingsPane.setVisible(false);
+        trainingPane.setVisible(true);
+
+    }
+
+    public void goToSettings(ActionEvent event){
+
+        trainingSettingsPane.setVisible(true);
+        trainingPane.setVisible(false);
+
+    }
+
     public void changeCardsOfCategory(ActionEvent event){
 
         String category = categoryOfCardBox.getValue().toString();
-        deskQuestionImage.setImage(new Image("images/question.png"));
+        deskQuestionImage.setImage(new Image(startPath));
         if(category.equalsIgnoreCase("all"))
             tableViev.setItems(new Desk().setCardsList());
         else
@@ -174,19 +213,27 @@ public class Controller implements Initializable {
     }
 
     public void setAllWidgetsUnvisible(){
-        this.variantA_Button.setVisible(false);
-        this.variantB_Button.setVisible(false);
-        this.variantC_Button.setVisible(false);
-        this.variantD_Button.setVisible(false);
+        this.gridForButtons.setVisible(false);
         this.answerTextArea.setVisible(false);
         this.resultText.setVisible(false);
     }
 
     public void setDesk()
     {
-        backTableColumn.setCellValueFactory(new PropertyValueFactory<Card, String>("word"));
-        frontTableColumn.setCellValueFactory(new PropertyValueFactory<Card, String>("definition"));
-//        tableViev.setItems(new Desk().setCardsList("animals"));
+
+        frontTableColumn.setCellValueFactory(cellData -> (new SimpleStringProperty()));
+        frontTableColumn.setCellFactory(param -> {
+            TableCell<Card, String> cell = new TableCell<>();
+            Text text = new Text();
+            text.setStyle("");
+            cell.setGraphic(text);
+            cell.setPrefHeight(Control.USE_COMPUTED_SIZE);
+            text.textProperty().bind(cell.itemProperty());
+            text.wrappingWidthProperty().bind(frontTableColumn.widthProperty());
+            return cell;
+        });
+        backTableColumn.setCellValueFactory(new PropertyValueFactory<>("word"));
+        frontTableColumn.setCellValueFactory(new PropertyValueFactory<>("definition"));
         tableViev.setItems(new Desk().setCardsList());
     }
 
@@ -195,4 +242,31 @@ public class Controller implements Initializable {
         categoryOfCardBox.setItems(new Desk().setCategoryList());
     }
 
+    public void openImageDesk(MouseEvent event){
+
+        Alert dialog = new Alert(Alert.AlertType.NONE);
+        dialog.initStyle(StageStyle.UTILITY);
+        dialog.setTitle("IMAGE");
+
+        Card setCard;
+        setCard = tableViev.getSelectionModel().getSelectedItem();
+        ImageView img;
+        if(setCard == null)
+            img = new ImageView(new Image(startPath));//deskQuestionImage;
+        else
+            img = new ImageView(new Image(setCard.getPicturePath().substring(19)));//deskQuestionImage;
+
+        img.setPreserveRatio(true);
+        img.setFitHeight(700);
+        img.setFitWidth(500);
+
+        BorderPane pane = new BorderPane();
+        pane.setPrefSize(300, 400);
+        pane.setCenter(img);
+
+        dialog.getDialogPane().getButtonTypes().add(ButtonType.CLOSE);
+        dialog.getDialogPane().setGraphic(pane);
+
+        Optional<ButtonType> r = dialog.showAndWait();
+    }
 }
